e795d1f23f796ffbebc25ee43a04c0eab69147df
Greg Fullard <gregf@8bitplatoon.com>
2018-05-06 10:45:31 +0200

    Commenting out the launch command

--- testing/bdd/bdd-demo/src/test/java/com/argilla/demo/pages/DeveloperManualTocPage.java
+++ testing/bdd/bdd-demo/src/test/java/com/argilla/demo/pages/DeveloperManualTocPage.java
@@ -32,10 +32,6 @@ public class DeveloperManualTocPage extends PageObject {
 	public List<String> getEntries() {
 		System.out.println("Build List of all ToC entries:");
 		
-		//WebElementFacade entryList = find(By.tagName("ul"));
-		//return entryList.findElements(By.cssSelector("li.toctree-l1")).stream().map(element -> element.getText())
-		//		.collect(Collectors.toList());
-		
 		List<String> tocEntriesList = new ArrayList<String>();
 		
 		List<WebElementFacade> ulElementsList = findAll(By.cssSelector("li.toctree-l1"));
@@ -49,25 +45,4 @@ public class DeveloperManualTocPage extends PageObject {
 		
 		return tocEntriesList;
 	}
-
-	// @FindBy(name="search")
-	// private WebElementFacade searchTerms;
-	//
-	// @FindBy(name="go")
-	// private WebElementFacade lookupButton;
-	//
-	// public void enter_keywords(String keyword) {
-	// searchTerms.type(keyword);
-	// }
-	//
-	// public void lookup_terms() {
-	// lookupButton.click();
-	// }
-	//
-	// public List<String> getDefinitions() {
-	// WebElementFacade definitionList = find(By.tagName("ol"));
-	// return definitionList.findElements(By.tagName("li")).stream()
-	// .map( element -> element.getText() )
-	// .collect(Collectors.toList());
-	// }
 }
