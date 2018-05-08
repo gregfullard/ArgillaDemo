762480a05f831704db34fc3bbed88d0201af9f8d
Greg Fullard <gregf@8bitplatoon.com>
2018-05-08 07:34:19 +0200

    Adding ASCII Art

--- src/node/node-drone-app/server.js
+++ src/node/node-drone-app/server.js
@@ -24,4 +24,16 @@ var server = app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 server.close();
 
+function sayHowzit() {
+	console.log("         _  _   _____                                _   _     _                ");
+	console.log("	    _| || |_|  __ \             /\               | | | |   (_)               ");
+	console.log("	   |_  __  _| |  | | _____   __/  \   _ __  _   _| |_| |__  _ _ __   __ _    ");
+	console.log("	    _| || |_| |  | |/ _ \ \ / / /\ \ | '_ \| | | | __| '_ \| | '_ \ / _` |   ");
+	console.log("	   |_  __  _| |__| |  __/\ V / ____ \| | | | |_| | |_| | | | | | | | (_| |   ");
+	console.log("	     |_||_| |_____/ \___| \_/_/    \_\_| |_|\__, |\__|_| |_|_|_| |_|\__, |   ");
+	console.log("	                                             __/ |                   __/ |   ");
+	console.log("	                                            |___/                   |___/    ");
+	return true;
+}
+
 // To check drone logs: docker logs env_node-drone-app_1
